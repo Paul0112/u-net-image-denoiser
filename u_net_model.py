217f59ec4b94446197b6ee4346e832bd5752f412
Paul0112 <177804960+Paul0112@users.noreply.github.com>
2026-08-04 14:11:23 -0400

    U-net implemented

---
+++ u_net_model.py
@@ -0,0 +1,49 @@
+from u_net_utils import *
+
+# Implementation of U_net with the architecture defined in utils
+
+class U_net(nn.Module):
+    def __init__(self, in_channels, out_channels, num_levels):
+        super().__init__()
+
+        self.map_features_list = []
+        self.encoders = nn.ModuleList()
+        self.decoders = nn.ModuleList()
+
+        curr_in = in_channels
+        curr_out = out_channels
+
+        # Instance the encoders levels
+        for _ in range(num_levels):
+            feat = Feature(curr_in, curr_out)
+            self.encoders.append(Encoder(feat, self.map_features_list))
+            curr_in = curr_out
+            curr_out = curr_out * 2
+
+        # U shape
+        self.bottom = Feature(curr_in, curr_out)
+
+        # Instance the decoders levels
+        for _ in range(num_levels):
+            feat = Feature(curr_out, curr_in)
+            self.decoders.append(Decoder(feat, self.map_features_list))
+            curr_out = curr_in
+            curr_in = curr_in // 2
+
+        self.output_layer = nn.Conv2d(out_channels, 1, kernel_size=1)
+
+
+    def forward(self, x):
+        self.map_features_list.clear()
+
+        for encoder in self.encoders: # down
+            x = encoder(x)
+
+        x = self.bottom(x) # bottom
+
+        for decoder in self.decoders: # up
+            x = decoder(x)
+
+        logits = self.output_layer(x)
+        return logits
+
--- u_net_utils.py
+++ u_net_utils.py
@@ -1,32 +1,29 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+import torchvision.transforms.functional as TF
 
-"""
-Implementation of U-net convolutional architecture
-"""
-
+# Implementation of U-net architecture
 
 class Feature(nn.Module):
     '''
     Define the convolutional block to extract features
-    in each 
+    in each level of encode and decode layers.
     '''
 
-    def __init__(self, in_channels, out_channels, kernel_size):
+    def __init__(self, in_channels, out_channels):
         super().__init__()
-        features = out_channels
 
-        # we use the braai architecture 
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+    
         self.convolutional_block = nn.Sequential(
-            nn.Conv2d(in_channels, features, kernel_size=kernel_size),
-            nn.ReLU(),
-            nn.Conv2d(features, features * 2, kernel_size=kernel_size),
-            nn.ReLU()
+            nn.Conv2d(in_channels, out_channels, kernel_size= 3),
+            nn.ReLU(inplace= True),
+            nn.Conv2d(out_channels, out_channels, kernel_size= 3),
+            nn.ReLU(inplace= True)
         )
-        self.num_filters = features * 2
-        self.kernel_size = kernel_size
-        self.max_pool = nn.MaxPool2d(2, 2)
+
+        self.num_filters = out_channels * 2
         self.dropout = nn.Dropout(0.2)
 
 
@@ -36,83 +33,55 @@ class Feature(nn.Module):
 
 
 class Encoder(nn.Module):
+    '''
+    Define the encode level of the architecture; apply the convolutional block of Feature
+    (save the current features) and max_pool and dropout to the layer.
+    '''
     def __init__(self, feature_block, features_list): # feature block is a instance of Feature
         super().__init__()
 
-        # we use the braai architecture for the encoder path
         self.current_level = feature_block
         self.map_features = features_list
+        self.max_pool = nn.MaxPool2d(kernel_size= 2, stride= 2)
+
 
     def forward(self, x):
-        x = self.current_level.convolutional_block(x)
-        self.map_features.add(x)
-        
-        x = self.current_level.max_pool(x)
+        x = self.current_level(x)
+        self.map_features.append(x)
+        x = self.max_pool(x)
         x = self.current_level.dropout(x)
-
 
         return x
 
 
 class Decoder(nn.Module):
+    '''
+    Define the decode level of the architecture; apply upsampling (via ConvTranspose2d), load and crop
+    the skip connections saved in features_list (Encoder path) and finally apply the convolutional block 
+    of Feature and dropout to the layer.
+    '''
 
     def __init__(self, feature_block, features_list): # feature block is a instance of Feature
         super().__init__()
 
-        # we use the braai architecture for the encoder path
-        n_features = self.current_level.num_filters
-        kernel_size = self.current_level.kernel_size
-
         self.current_level = feature_block
         self.map_features = features_list
-        self.resize = nn.ConvTranspose2d(in_channels= n_features, out_channels= n_features / 2, 
-                                         kernel_size= kernel_size, stride= kernel_size)
+        self.resize = nn.ConvTranspose2d(in_channels= self.current_level.in_channels, 
+                                         out_channels= self.current_level.in_channels // 2, 
+                                         kernel_size= 2, stride= 2)
         
-
     def forward(self, x):
         x = self.resize(x)
+
         current_feature_skip = self.map_features.pop()
-        skip_cropped = F.interpolate(current_feature_skip, scale_factor=2, mode="bilinear", align_corners=False)
-        x = torch.cat([x, skip_cropped], dim= 1)
-        
-        x = self.feature_block.forward(x)
+        skip_cropped = TF.center_crop(current_feature_skip, output_size= x.shape[2:])
+
+        x = torch.cat([skip_cropped, x], dim= 1)
+        x = self.current_level(x)
         x = self.current_level.dropout(x)
 
         return x
 
-
-
-class U_net(nn.Module):
-    '''
-    Define the convolutional block to extract features
-    in each 
-    '''
-    def __init__(self, in_channels, out_channels, kernel_size, num_layers):
-        super().__init__()
-
-        self.map_features_list = []
-        self.num_layers = num_layers
-        self.in_channels = in_channels
-        self.out_channels = out_channels
-        self.kernel_size = kernel_size
-
-        self.convolutional_block = Feature(in_channels, out_channels, kernel_size)
-        self.encoder = Encoder(self.convolutional_block, self.map_features_list)
-        self.decoder = Decoder(self.convolutional_block, self.map_features_list)
-        self.output_layer = nn.Conv2d(in_channels, out_channels, kernel_size=1)
-
-
-    def forward(self, x):
-
-        for _ in (self.num_layers + 1):
-            x = Encoder(x)
-
-        for _ in (self.num_layers):
-            x = Decoder(x)
-
-        logits = self.output_layer(x)
-        return logits
-        
 
 
 
